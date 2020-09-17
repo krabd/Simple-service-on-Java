ade6c0820aa306a0df415a9f635dc2ccede4ad07
krab <dimaandrievskiy@mail.ru>
2020-09-18 01:01:09 +0300

    рефакторинг роутинга

--- src/main/java/com/krab/rest/controller/RecordController.java
+++ src/main/java/com/krab/rest/controller/RecordController.java
@@ -10,32 +10,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/records")
 public class RecordController {
 
-    private RecordsService recordsService;
+    private final RecordsService recordsService;
 
     @Autowired
     public RecordController(RecordsService recordsService) {
         this.recordsService = recordsService;
     }
 
-    @PostMapping("/api/addRecord")
+    @PostMapping("/add")
     public ResponseEntity<Record> addRecord(@RequestParam String description, @RequestParam long authorId) throws ResourceNotFoundException {
         return ResponseEntity.ok(recordsService.addRecord(authorId, description));
     }
 
-    @PutMapping("/api/updateRecord/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<Record> updateRecord(@RequestBody Record record, @PathVariable long id, @RequestParam long authorId) throws ResourceNotFoundException {
         return ResponseEntity.ok(recordsService.updateRecord(id, authorId, record.getDescription()));
     }
 
-    @GetMapping("/api/getRecordsByAuthorId")
-    public ResponseEntity<List<Record>> getRecordByAuthorId(@RequestParam long authorId) {
+    @GetMapping("/getByAuthorId")
+    public ResponseEntity<List<Record>> getRecordsByAuthorId(@RequestParam long authorId) {
         return ResponseEntity.ok(recordsService.getRecordsByAuthorId(authorId));
     }
 
-    @GetMapping("/api/getRecordsByAuthorFirstName")
-    public ResponseEntity<List<Record>> getByAuthorFirstName(@RequestParam String authorFirstName) {
+    @GetMapping("/getByAuthorFirstName")
+    public ResponseEntity<List<Record>> getRecordsByAuthorFirstName(@RequestParam String authorFirstName) {
         return ResponseEntity.ok(recordsService.getRecordsByAuthorFirstName(authorFirstName));
     }
 }
