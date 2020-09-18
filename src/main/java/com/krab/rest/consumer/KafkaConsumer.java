98f6ba3fab46b4b8f65d28657d537d7e4099b451
krab <dimaandrievskiy@mail.ru>
2020-09-18 14:41:14 +0300

    логгер в консьюмере

--- src/main/java/com/krab/rest/consumer/KafkaConsumer.java
+++ src/main/java/com/krab/rest/consumer/KafkaConsumer.java
@@ -1,11 +1,22 @@
 package com.krab.rest.consumer;
 
 import com.krab.rest.dto.AuthorDto;
+import com.krab.rest.services.AuthorsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class KafkaConsumer {
+
+    private final AuthorsService authorsService;
+
+    @Autowired
+    public KafkaConsumer(AuthorsService authorsService) {
+        this.authorsService = authorsService;
+    }
 
     @KafkaListener(topics = "messages", groupId = "group_id")
     public void consumeMessages(String message) {
@@ -14,6 +25,7 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "addAuthor", groupId = "group_author_id", containerFactory = "concurrentKafkaListenerContainerFactoryAuthor")
     public void consumeAuthors(AuthorDto author) {
-        System.out.println("message = " + author);
+        log.info("Add author from MQ Value = " + author);
+        authorsService.addAuthor(author.getFirstName(), author.getLastName());
     }
 }
